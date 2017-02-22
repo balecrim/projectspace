40d9b9ad1e91a3e4bdeb6df1c35efcd6b0079f59
Bernardo Alecrim <bwalecrim@gmail.com>
2017-02-21 23:38:36 -0400

    Introducing SKBalloonNode + Tile information
    Signed-off-by: Bernardo Alecrim <bwalecrim@gmail.com>

---
+++ SKBalloonNode.swift
@@ -0,0 +1,9 @@
+//
+//  SKBalloonNode.swift
+//  TechDemo
+//
+//  Created by Bernardo Alecrim on 2/21/17.
+//  Copyright © 2017 Bernardo Alecrim. All rights reserved.
+//
+
+import Foundation
---
+++ TechDemo/UIView+toImage.swift
@@ -0,0 +1,9 @@
+UI//
+//  File.swift
+//  TechDemo
+//
+//  Created by Bernardo Alecrim on 2/21/17.
+//  Copyright © 2017 Bernardo Alecrim. All rights reserved.
+//
+
+import Foundation
